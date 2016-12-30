1fac0f6169be5575c8d5b92613f92991fe305c90
Sagang Wee <sagangwee@yahoo.com>
2016-12-30 02:07:45 -0800

    added grid to contact

--- src/js/pages/Contact.js
+++ src/js/pages/Contact.js
@@ -1,10 +1,22 @@
 import React from "react";
+import {Grid, Row, Column} from 'react-cellblock';
 
 export default class Contact extends React.Component {
   render() {
     return (
-      <h1>Contact</h1>
-      
+      <div>
+      	<h1>Contact</h1>
+      	<Grid>
+  	      <Row>
+  	        <Column width="1/2">
+  	          Left!
+  	        </Column>
+  	        <Column width="1/2">
+  	          Right!
+  	        </Column>
+  	      </Row>
+      	</Grid>
+      </div>
     );
   }
 }

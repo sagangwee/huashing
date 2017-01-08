cf71a5bf86d6d32e76b4b0d7a5b441390343dd92
Sagang Wee <sagangwee@yahoo.com>
2017-01-08 00:32:34 -0800

    Added reviewers to testimonials

--- src/js/components/Testimonial.js
+++ src/js/components/Testimonial.js
@@ -1,13 +1,17 @@
 import React from "react";
 
-export default class Article extends React.Component {
+export default class Testimonial extends React.Component {
   render() {
     const { text } = this.props;
+    const { reviewer } = this.props;
 
     return (
       <div class="testimonial">
         <p>
           <em>"{text}"</em>
+          <div class="reviewer">
+            <strong>— { reviewer }</strong>
+          </div>
         </p>
       </div>
     );
